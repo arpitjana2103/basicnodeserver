d7720bed4391b759c1c4f151c002ae9cb486315e
arpit jana <arpitjana2103@gmail.com>
2026-08-23 20:51:29 +0530

    update index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import express from "express";
 const app = express();
 app.get("/", (req, res) => {
     return res.status(200).json({
-        message: "Hello from Server",
+        message: "Hello from Server v2",
     });
 });
 
